228d0021c12be95e76826d0cd3609751aad7227a
Sarvjeet Yadav <your-email@example.com>
2026-06-08 10:23:11 +0530

    chore: update resume link to latest version
    - Update resume Google Drive link from 1FppEHa5nNbqqks_QAOLvO24rj5iPcBao to 1Zvinh6Fzt1iWPnoGvVM6WWIBrR_LOtZK

--- src/components/dom/Navigation.tsx
+++ src/components/dom/Navigation.tsx
@@ -17,7 +17,7 @@ const Navigation = memo(() => {
     { label: 'Projects', href: '#projects' },
     { label: 'Skills', href: '#skills' },
     { label: 'Contact', href: '#contact' },
-    { label: 'Resume', href: 'https://drive.google.com/file/d/1FppEHa5nNbqqks_QAOLvO24rj5iPcBao/view?usp=sharing', external: true },
+    { label: 'Resume', href: 'https://drive.google.com/file/d/1Zvinh6Fzt1iWPnoGvVM6WWIBrR_LOtZK/view?usp=sharing', external: true },
   ];
 
   const scrollToSection = (href: string, external?: boolean) => {
